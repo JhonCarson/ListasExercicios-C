fc1edc0f01d5ecb74e7ca8a3647139b7a96d6be9
JhonCarson <140865490+JhonCarson@users.noreply.github.com>
2023-08-17 18:39:10 -0300

    Updating Files

--- ListaDeExercicio-04-08-23-CP2/Exercicio1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-    printf("Inicio do programa\n");
-    printf("Fim do programa");
-
-    return 0;
-}
--- ListaDeExercicio-04-08-23-CP2/Exercicio11.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define constante 1.5f
-
-int main() {
-
-    printf("Segue o valor da constante float: %.2f", constante);
-
-    return 0;
-}
