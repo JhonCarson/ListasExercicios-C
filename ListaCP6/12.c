24c5fe1280aa25d11d9258b2abda47b291b2f82d
JhonCarson <140865490+JhonCarson@users.noreply.github.com>
2023-08-31 11:03:42 -0300

    atualização

--- ListaCP6/12.c
+++ ListaCP6/12.c
@@ -5,21 +5,31 @@ dessa matriz que estão abaixo da diagonal principal.*/
 
 int main() {
 
-    int m[5][5], soma = 0;
+    int m[6][6], soma = 0;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 6; j++) {
             
             printf("\nDigite a valor da linha %d e coluna %d: ", i +1, j +1);
             scanf(" %d", &m[i][j]);
 
-            if (i < j && j == i +1) {
+            if (i  == j +1) {
              
                 soma += m[i][j];
             }
         }    
     }
+    for (int i = 0; i < 6; i++) {
+        
+        for (int j = 0; j < 6; j++) {
+                
+            printf("%d ", m[i][j]);
+        }    
+        printf("\n");
+    }
+    printf("\nA soma dos numeros abaixo da diagonal principal eh: %d", soma);
+    printf("\n ");
 
     return 0;
 }
